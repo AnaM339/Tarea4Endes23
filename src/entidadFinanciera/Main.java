e5722290c4d1fbe43114d2f247bf67ed85b2329f
anama <anama@Ana>
2023-03-14 09:25:23 +0100

    Cambios realizados: - se ha creado un atributo constante ERRORCANTIDADNEGATIVA - se han introducido los métodos probarIngresar y probarRetirar en la clase Main - se han encapsulado los atributos de la clase CCuenta - se ha añadido el parámetro cantidadEsperada a los métodos probarRetirar y probarIngresar

--- src/entidadFinanciera/Main.java
+++ src/entidadFinanciera/Main.java
@@ -9,23 +9,40 @@ package entidadFinanciera;
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        CCuenta objetoCuenta;
         double saldoActual;
         
 
-        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = miCuenta.estado();
+        objetoCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = objetoCuenta.estado();
         System.out.println("El saldo actual es"+ saldoActual );
         
     
-    }
-    
-    public static void probarIngresar(CCuenta c, int cantidad) throws Exception{
-        c.ingresar(cantidad);
+        try {
+            probarIngresar(objetoCuenta, 500, 500);
+        } catch (Exception e) {
+            System.out.println("Error al ingresar dinero: " + e.getMessage());
+        }
         
+        try {
+            probarRetirar(objetoCuenta, 400, 400);
+        } catch (Exception e) {
+            System.out.println("Error al retirar dinero: " + e.getMessage());
+        }
+    }
+       
+    public static void probarIngresar(CCuenta c, int cantidad, int cantidadEsperada) throws Exception{
+        c.ingresar(cantidad);
+        double saldoActual = c.estado();
+        if (saldoActual != cantidadEsperada){
+            throw new Exception("La cantidad esperada no coincide con el saldo actual");
+        }
     }
     
-    public static void probarRetirar(CCuenta c, int cantidad) throws Exception{
+    public static void probarRetirar(CCuenta c, int cantidad, int cantidadEsperada) throws Exception{
         c.retirar(cantidad);
+        double saldoActual = c.estado();
+        if (saldoActual != cantidadEsperada){
+            throw new Exception("La cantidad esperada no coincide con el saldo actual");
     }
 }
